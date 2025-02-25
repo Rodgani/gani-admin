dfc3e6dc464ddc15c976a769f58e8f87b9781a4e
Gani <rod.ganancial@gmail.com>
2025-02-25 21:04:50 +0800

    first commit

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,6 +13,11 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Minsan',
+        url: '/dashboard',
+        icon: LayoutGrid,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
