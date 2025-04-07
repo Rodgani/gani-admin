2079e370e390725bd14c99b1e19748de505bf023
Gani <rod.ganancial@gmail.com>
2025-04-07 23:48:54 +0800

    feat: convert to arrow function

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', function () {
-        return Redirect::to('/login');
-    })->name('login');
 
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('/', fn() => Redirect::to('/login'))
+        ->name('login');
+
+    Route::get('dashboard', fn() => Inertia::render('dashboard'))
+        ->name('dashboard');
 
     require __DIR__ . '/settings.php';
     require __DIR__ . '/users.php';
