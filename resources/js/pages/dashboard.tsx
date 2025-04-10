55b3d65de31c67bf20f22c4aced8720bb567a716
Gani <rod.ganancial@gmail.com>
2025-04-10 17:55:36 +0800

    dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -34,6 +34,9 @@ export default function Dashboard() {
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                </div>
             </div>
 
         </AppLayout>
